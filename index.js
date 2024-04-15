2deea76c5b71b68c148997e6c475c10e9294f409
Naveenkumar <121660045+naveenkumarr-onyx@users.noreply.github.com>
2024-04-15 23:13:07 +0530

    update

--- index.js
+++ index.js
@@ -1,66 +1,33 @@
 function addTask() {
   var taskInput = document.getElementById("task").value;
-  var tasks = JSON.parse(localStorage.getItem("taskInput")) || [];
+  if (!taskInput) return;
+  var tasks = getTasksFromStorage() || [];
   tasks.push(taskInput);
-  localStorage.setItem(taskInput, JSON.stringify(tasks));
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  document.getElementById("task").value = "";
   display(tasks);
 }
-function display(listName) {
-  var listContainer = document.getElementById("taskList");
-  listContainer.innerHTML = listName;
+
+function display(tasks) {
+  var taskListContainer = document.getElementById("taskList");
+  taskListContainer.innerHTML = "";
+  tasks.map((task) => {
+    var li = document.createElement("li");
+    li.textContent = task;
+    taskListContainer.appendChild(li);
+  });
 }
-
-// function display(listName) {
-//   var taskListContainer = document.getElementById("taskList");
-//   taskListContainer.innerHTML = "";
-//   var ul = document.createElement("ul");
-
-//   listName.forEach((task) => {
-//     var listItem = document.createElement("li");
-//     task.textContent = listItem;
-//     ul.appendChild(listItem);
-//   });
-//   taskListContainer.appendChild(ul);
-// }
-// window.onload = () => {
-//   display("default");
-// };
-
-// document.addEventListener("DOMContentLoaded", () => {
-//   let addButton = document.getElementById("btn");
-//   let addInput = document.getElementById("task");
-
-//   addButton.addEventListener("click", function () {
-//     // grab the value of the input tag
-//     let newItem = document.getElementById("item").value;
-//     // if the input tag is not empty then run our function to add an item
-//     if (newItem) {
-//       // this function will add a new item to the todo list
-//       addItemTodo(newItem);
-//       // reset the input after we added a new item
-//       document.getElementById("item").value = "";
-//     }
-//   });
-//   // user press enter
-//   addInput.addEventListener("keypress" || "click", function (e) {
-//     console.log(e);
-//     // did the user press *enter*? if yes then continue
-//     if (13 === e.keyCode) {
-//       // grab the value of the input tag
-//       let newItem = document.getElementById("item").value;
-//       // if the input tag is not empty then run our function to add an item
-//       if (newItem) {
-//         // this function will add a new item to the todo list
-//         addItemTodo(newItem);
-//         // reset the input after we added a new item
-//         document.getElementById("item").value = "";
-//       }
-//     }
-//   });
-
-//   function addItemTodo(text) {
-//     let list = document.getElementById("todo");
-//     let item = document.createElement("li");
-//     list.innerHTML = text;
-//   }
-// });
+function getTasksFromStorage() {
+  return JSON.parse(localStorage.getItem("tasks")) || [];
+}
+window.onload = () => {
+  var tasks = getTasksFromStorage();
+  if (tasks) {
+    display(tasks);
+  }
+};
+function clearStorage() {
+  var removeItem = localStorage.removeItem("tasks");
+  window.onload();
+  return removeItem;
+}
